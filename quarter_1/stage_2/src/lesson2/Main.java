9eeca5c4da20983f4a496e0b668d22c423ca78dc
Anton-mtd <anton_mtd1055@mail.ru>
2021-12-27 12:07:57 +0300

    homeTask_1-2.2_rectify_1

--- quarter_1/stage_2/src/lesson2/Main.java
+++ quarter_1/stage_2/src/lesson2/Main.java
@@ -1,26 +1,37 @@
 package lesson2;
 
 public class Main {
+
+    private static final int REQUIRED_SIZE = 4;
+
     public static void main(String[] args) {
-        String [][] array1 = createAndFillArrayWithData(4,4, "1");
+        String[][] array1 = createAndFillArrayWithData(4, 4, "1");
         String[][] array2 = createAndFillArrayWithData(5, 5, "2");
         String[][] array3 = createAndFillArrayWithData(4, 4, "1");
-        array3 [1][3] = "a";
-        array3 [0][2] = "g";
+        array3[1][3] = "a";
+        array3[0][2] = "g";
 
-        sumValues(array1);
-        System.out.println("________________");
-        sumValues(array2);
-        System.out.println("________________");
-        sumValues(array3);
+        try {
+            sumValues(array1);
+            System.out.println("________________");
+            sumValues(array2);
+        } catch (MyArraySizeException | MyArrayDataException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            sumValues(array3);
+        } catch (MyArraySizeException | MyArrayDataException ex) {
+            System.out.println(ex.getMessage());
+        }
 
         System.out.println("Программа завершена!");
     }
 
-    public static String[][] createAndFillArrayWithData(int column, int row, String number){
-        String [][] stringArray = new String[column][row];
-        for (int i = 0; i <stringArray.length ; i++) {
-            for (int j = 0; j < stringArray[i].length ; j++) {
+    private static String[][] createAndFillArrayWithData(int column, int row, String number) {
+        String[][] stringArray = new String[column][row];
+        for (int i = 0; i < stringArray.length; i++) {
+            for (int j = 0; j < stringArray[i].length; j++) {
                 stringArray[i][j] = number;
             }
         }
@@ -28,51 +39,34 @@ public class Main {
     }
 
 
-    public static boolean checkArray(String[][] array) {
-        try {
-            if (array.length != 4) {
-                throw new MyArraySizeException();
-            } else {
-                for (String[] string : array) {
-                    if (string.length != 4) {
-                        throw new MyArraySizeException();
-                    }
-                }
-            }
-        } catch (MyArraySizeException myArrayEx) {
-            System.out.println(myArrayEx.getMessage());
-            return false;
+    private static void checkArray(String[][] array) {
+        if (array.length != REQUIRED_SIZE) {
+            throw new MyArraySizeException();
         }
-        return true;
+        for (String[] string : array) {
+            if (string.length != REQUIRED_SIZE) {
+                throw new MyArraySizeException();
+            }
+        }
     }
 
 
     private static void sumValues(String[][] array) {
-        if (checkArray(array)) {
-            int sum = 0;
-            int number = 0;
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    if (!isNumber(array[i][j])) {
-                        try {
-                            throw new MyArrayDataException();
-                        } catch (MyArrayDataException MyDataEx) {
-                            MyDataEx.defineWrongData(i, j);
-                        }
-                    } else {
-                        number = Integer.parseInt(array[i][j]);
-                        sum += number;
-                    }
+        checkArray(array);
+        int sum = 0;
+        int number = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    number = Integer.parseInt(array[i][j]);
+                    sum += number;
+                } catch (NumberFormatException ex) {
+                    throw new MyArrayDataException(i, j);
                 }
             }
-            System.out.println("Сумма всех чисел в двумерном массиве равна " + sum);
         }
+        System.out.println("Сумма всех чисел в двумерном массиве равна " + sum);
     }
 
-
-    public static boolean isNumber (String str){
-        return str.matches("\\d+");
-    }
-    
 }
 
--- quarter_1/stage_2/src/lesson2/MyArrayDataException.java
+++ quarter_1/stage_2/src/lesson2/MyArrayDataException.java
@@ -1,11 +1,8 @@
 package lesson2;
 
 public class MyArrayDataException extends RuntimeException {
-    public MyArrayDataException() {
-        super("Wrong data");
-    }
+    public MyArrayDataException(int column, int row) {
+        super(String.format("В ячейке %s , %s не верный формат. Не число!%n", column, row));
 
-    public void defineWrongData(int column, int row){
-        System.out.printf("В ячейке %s , %s не верный формат. Не число!%n", column, row);
     }
 }
