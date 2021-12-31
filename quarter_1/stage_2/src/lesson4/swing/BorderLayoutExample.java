45e89e3fd31053ad9c5a366c4dd6ff62a776cb37
Anton-mtd <anton_mtd1055@mail.ru>
2021-12-31 19:46:10 +0300

    homeTask_1-2.4

---
+++ quarter_1/stage_2/src/lesson4/swing/BorderLayoutExample.java
@@ -0,0 +1,30 @@
+package lesson4.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BorderLayoutExample {
+
+    public static void main(String[] args) {
+        new MyWindowBorder();
+    }
+}
+
+class MyWindowBorder extends JFrame {
+    MyWindowBorder() {
+        setTitle("MyWindow");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 400, 400);
+        JButton[] jButtons = new JButton[5];
+        for (int i = 0; i < 5 ; i++) {
+            jButtons[i] = new JButton("#" + i);
+        }
+        setLayout(new BorderLayout());
+        add(jButtons[0], BorderLayout.EAST);
+        add(jButtons[1], BorderLayout.WEST);
+        add(jButtons[2], BorderLayout.SOUTH);
+        add(jButtons[3], BorderLayout.NORTH);
+        add(jButtons[4], BorderLayout.CENTER);
+        setVisible(true);
+    }
+}
